42a334632d29ff49e1f2f47fa804304348054905
mateenparkar <mateenparkar4@gmail.com>
2024-01-09 11:26:44 +0000

    View book on a separate page

--- src/controller/BooksController.ts
+++ src/controller/BooksController.ts
@@ -1,14 +1,25 @@
 import { Request, Response } from "express";
-import { viewBooks } from "../service/booksService";
+import { viewBook, viewBooks } from "../service/booksService";
 
 
 export class BooksController {
-    public static get = async function(req:Request, res:Response): Promise<void>{
+    public static getAll = async function(req:Request, res:Response): Promise<void>{
         try{
             const books = await viewBooks();
             res.render('books.html', {books:books});
         }catch(e){
             console.error(e);
         }
-    }
+    };
+
+    public static getOne = async function(req:Request, res:Response): Promise<void>{
+        try{
+            const id = parseInt(req.params.id, 10); 
+            const book = await viewBook(id);
+            console.log(book)
+            res.render('book_detail.html', {books:book});
+        }catch(e){
+            console.error(e);
+        }
+    };
 }
--- src/service/booksService.ts
+++ src/service/booksService.ts
@@ -34,3 +34,27 @@ export const viewBooks = async function (): Promise<BookWithAuthor[]> {
         throw new Error('Could not get books with authors');
     }
 };
+
+
+export const viewBook = async function (id: number): Promise<BookWithAuthor> {
+    try {
+        const bookResponse = await axios.get('http://localhost:8080/api/books/' + id);
+        const book: Books = bookResponse.data;
+
+        const authorResponse = await axios.get(`http://localhost:8080/api/author/${book.author}`);
+        const authorName: string = authorResponse.data.name;
+
+        const genreResponse = await axios.get(`http://localhost:8080/api/genre/${book.genre}`);
+        const genreName: string = genreResponse.data.genre_name;
+
+        // Convert published_date from timestamp to a readable date format (day/month/year)
+        const formattedPublishedDate: string = new Date(book.published_date)
+            .toLocaleDateString(undefined, { day: 'numeric', month: 'numeric', year: 'numeric' });
+
+        const bookWithAuthor: BookWithAuthor = { ...book, authorName, formattedPublishedDate, genreName };
+        return bookWithAuthor;
+
+    } catch (error) {
+        throw new Error('Could not get book with author');
+    }
+};
