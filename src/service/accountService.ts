67d715612cc5fcc9a100d4439e7132d8f519153f
mateenparkar <mateenparkar4@gmail.com>
2024-08-10 09:33:22 +0100

    implement front end logic for changing password

---
+++ src/service/accountService.ts
@@ -0,0 +1,12 @@
+import { Update } from "../model/auth";
+import axios, { AxiosError } from "axios";
+
+export const updatePassword = async function(update:Update): Promise<void>{
+    try{
+        await axios.put('http://localhost:8080/api/account', update);
+    }catch(e){
+        if((e as AxiosError).response?.status === 400){
+            throw new Error('Failed to like book');
+        }
+    }
+}
